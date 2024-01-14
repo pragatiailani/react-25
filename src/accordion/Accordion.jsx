7346ff1832de1c8919f2af5fa53a920b94ba3dfe
Pragati Aliani <pragatialiani2003@gmail.com>
2024-01-14 10:13:59 +0530

    single selection accordion close update

--- src/accordion/Accordion.jsx
+++ src/accordion/Accordion.jsx
@@ -5,33 +5,30 @@ import "./styles.css";
 function Accordion() {
   const [active, setActive] = React.useState([]);
   const [isSingleAccordion, setIsSingleAccordion] = React.useState(true);
-  
+
   const toggleAccordion = (index) => {
-    if (isSingleAccordion) {
-      setActive([index]);
+    if (active.includes(index)) {
+      setActive(active.filter((item) => item !== index));
     } else {
-      if (active.includes(index)) {
-        setActive(active.filter((item) => item !== index));
+      if (isSingleAccordion) {
+        setActive([index]);
       } else {
         setActive([...active, index]);
       }
     }
   };
-  
+
   const toggleAccordionType = () => {
     if (!isSingleAccordion) {
       setActive([]);
     }
     setIsSingleAccordion(!isSingleAccordion);
   };
-  
+
   return (
     <div className="accordion-main">
       <h1>Accordion</h1>
-      <button
-        className="accordion-type-toggle"
-        onClick={toggleAccordionType}
-      >
+      <button className="accordion-type-toggle" onClick={toggleAccordionType}>
         change to {isSingleAccordion ? "multiple" : "single"} selection
         accordion
       </button>
